c33787ae501dd8df8d92c3922a8b3ffdb58c2e59
arpit57 <arpit.singhal57@gmail.com>
2023-04-20 20:53:04 +0530

    storing similarities in sqlite

--- movieapp.py
+++ movieapp.py
@@ -2,17 +2,35 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
+import sqlite3
+
 st.title("Movie Recommender")
 
 movies = pickle.load(open("movie_list.pkl", "rb"))
 
-s0 = pickle.load(open("similarity.pkl_0.pkl", 'rb'))
-s1 = pickle.load(open("similarity.pkl_1.pkl", 'rb'))
-s2 = pickle.load(open("similarity.pkl_2.pkl", 'rb'))
-s3 = pickle.load(open("similarity.pkl_3.pkl", 'rb'))
-s4 = pickle.load(open("similarity.pkl_4.pkl", 'rb'))
+conn = sqlite3.connect('similarity.db')
+c = conn.cursor()
+c.execute("SELECT * FROM similarity")
+rows = c.fetchall()
 
-similarity = np.concatenate((s0, s1, s2, s3, s4))
+retrieved_similarity = []
+
+for row in rows:
+    row_id, data = row
+    numpy_row = np.frombuffer(data, dtype=float)
+    retrieved_similarity.append(numpy_row)
+
+similarity = np.array(retrieved_similarity)
+
+
+# s0 = pickle.load(open("similarity.pkl_0.pkl", 'rb'))
+# s1 = pickle.load(open("similarity.pkl_1.pkl", 'rb'))
+# s2 = pickle.load(open("similarity.pkl_2.pkl", 'rb'))
+# s3 = pickle.load(open("similarity.pkl_3.pkl", 'rb'))
+# s4 = pickle.load(open("similarity.pkl_4.pkl", 'rb'))
+
+# similarity = np.concatenate((s0, s1, s2, s3, s4))
+
 # similarity = pickle.load(open("similarity.pkl", 'rb'))
 # print(movies)
 selectedMovie = st.selectbox("", movies.title)
